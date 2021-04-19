3df7fa962f29dd27e231f82867f1fae7dc35f6ae
Brennan Wilkes <brennan@codexwilkes.com>
2021-04-18 18:18:36 -0700

    feat: Docker execute cmd

--- miscUtilities.sh
+++ miscUtilities.sh
@@ -27,5 +27,7 @@ echo 'alias editBashrc="gedit ~/.bashrc && source ~/.bashrc"' | addToFile
 echo 'kill-name () { proc="$1" ; ps -aux | grep "$proc" | tr -s " " | cut -d" " -f2 | xargs -n1 -I {} kill -9 {} ; }' | addToFile
 echo 'top-search() { top -c -p $(pgrep -d"," -f "$1") ; }' | addToFile
 echo 'search-files () { key="$1" ; grep -rnli . -e "$key" ; }' | addToFile
+echo 'dockerExecute () { query="$1"; shift; sudo docker exec $( sudo docker container ls | grep "$query" | cut -d" " -f1 ) $@ }' | addToFile
+
 
 grep -qxF 'filterByRegex()' "${1:-${BASHFILE}}" || echo 'filterByRegex() { while read line; do echo "$line" | grep -oE "$1" | sed -E "s/$1/\\1/g"; done ; }' >> "${1:-${BASHFILE}}"
