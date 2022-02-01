11bfc399a3e1878e9e25f97110f6caa62f69533e
Brennan Wilkes <brennan@codexwilkes.com>
2022-02-01 11:47:36 -0800

    Update uvic.sh

--- uvic.sh
+++ uvic.sh
@@ -16,13 +16,11 @@ addToFile(){
 	grep -qxF "$line" "$file" || echo "$line" >> "$file"
 }
 
-EC2=
-
 echo 'setupAwsCli() { curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" ; unzip awscliv2.zip ; rm awscliv2.zip ; ./aws/install -i /home/$HOME/aws-cli ; ~/aws-cli/v2/current/bin/aws configure ; }' | addToFile
 echo 'startEc2() { ~/aws-cli/v2/current/bin/aws ec2 start-instances --instance-ids i-0fb1b5499918be07f ; } ' | addToFile
 echo 'stopEc2() { ~/aws-cli/v2/current/bin/aws ec2 stop-instances --instance-ids i-0fb1b5499918be07f ; } ' | addToFile
 echo 'printEc2() { ~/aws-cli/v2/current/bin/aws ec2 describe-instances --filters Name=instance-id,Values=i-0fb1b5499918be07f --output text | grep -oE "ec2-[0-9-]*.us-west-.*.compute.amazonaws.com" | head -n1 ; } ' | addToFile
-echo 'alias ec2="~/aws-cli/v2/current/bin/aws ec2 describe-instances --filters Name=instance-id,Values=i-0fb1b5499918be07f --output text | grep -oE "ec2-[0-9-]*.us-west-.*.compute.amazonaws.com" | head -n1 | xargs -I {} ssh ubuntu@{}"' | addToFile
+echo 'ec2() { ssh ubuntu@`~/aws-cli/v2/current/bin/aws ec2 describe-instances --filters Name=instance-id,Values=i-0fb1b5499918be07f --output text | grep -oE "ec2-[0-9-]*.us-west-.*.compute.amazonaws.com" | head -n1` ; }' | addToFile
 
 
 #GIT Aliases (logg, st, p, etc)
